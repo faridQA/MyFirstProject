ccdb055c3d649b65f9a2de4f0add0697cac959fd
farid <farid@DESKTOP-2GE5789.home>
2019-03-17 12:58:39 -0400

    Hello friend come to church ave @9;30am

---
+++ AutomationTutorial/src/data/Constants.java
@@ -0,0 +1,10 @@
+package data;
+
+public class Constants {
+	
+	public static final String CHROMEDRIVER_PATH = "C:\\Users\\farid\\Documents\\chromedriver.exe";
+	public static final String BROWSER_TYPE = "chrome";//firefox, edge
+	public static final String BASE_URL = "http://www.demoaut.com";
+
+	public static final String DATAPOOL_PATH = "C:/Users/farid/Downloads/AutomationTutorial_Framework/AutomationTutorial/resource/Datapool.xls";
+}
